5e5b0a307ff9e1186b20d0cb2c6c31da1ac9bbd6
ketupia <ketupia@gmail.com>
2024-03-31 18:24:18 -0400

    finish Code Editors reading

--- git_push.sh
+++ git_push.sh
@@ -1,26 +1,22 @@
-#!/bin/zsh
+#!/bin/bash
 
 # Check for argument provided as commit message
-if [[ -z "$@" ]]; then
+if [ $# -eq 0 ]; then
   # No argument, prompt for message
   read -p "Enter commit message: " message
   # Exit if user enters nothing and presses enter
-  if [[ -z "$message" ]]; then
+  if [ -z "$message" ]; then
     echo "Error: Please provide a commit message."
     exit 1
   fi
 else
-  # Use the first argument as the message
+  # Use the argument as the message
   message="$1"
 fi
 
-# Add all changes
+# Rest of the script remains the same...
 git add .
-
-# Commit with the provided message
 git commit -m "$message"
-
-# Push to remote repository
 git push
 
 echo "Pushed code with message: '$message'"
